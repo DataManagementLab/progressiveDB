86da63eb83299314af72fa61372104350edba082
bergowic <ljberg@gmx.de>
2019-10-06 04:54:05 +0200

    [Core] Make PostgreSQL driver tests run with docker

--- core/src/test/java/de/tuda/progressive/db/driver/impl/PostgreSQLDriverTest.java
+++ core/src/test/java/de/tuda/progressive/db/driver/impl/PostgreSQLDriverTest.java
@@ -2,19 +2,38 @@ package de.tuda.progressive.db.driver.impl;
 
 import de.tuda.progressive.db.driver.PartitionDriver;
 import de.tuda.progressive.db.driver.PartitionDriverTest;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
 class PostgreSQLDriverTest extends PartitionDriverTest {
-  private static final String URL = "jdbc:postgresql://localhost:5432/progressive";
-  private static final String USER = "postgres";
-  private static final String PASSWORD = "postgres";
+
+  private static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:11")
+          .withDatabaseName("progressive")
+          .withUsername("postgres")
+          .withPassword("postgres");
 
   @BeforeAll
-  static void init() throws SQLException {
-    connection = DriverManager.getConnection(URL, USER, PASSWORD);
+  static void beforeAll() throws Exception {
+    postgreSQLContainer.start();
+
+    // PostgreSQL needs some time to start in container
+    Thread.sleep(1000);
+
+    connection = DriverManager.getConnection(
+            postgreSQLContainer.getJdbcUrl(),
+            postgreSQLContainer.getUsername(),
+            postgreSQLContainer.getPassword()
+    );
+  }
+
+  @AfterAll
+  static void afterAll() throws Exception {
+    connection.close();
+    postgreSQLContainer.stop();
   }
 
   @Override
